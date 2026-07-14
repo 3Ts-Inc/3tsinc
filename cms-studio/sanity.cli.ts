925168cca90fbed4f33b46bffceeb72028b6fce2
Hashem Nusair <nusairhashem04@gmail.com>
2026-07-14 14:12:42 +0300

    Pin Sanity Studio deployment

--- cms-studio/sanity.cli.ts
+++ cms-studio/sanity.cli.ts
@@ -5,4 +5,8 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: "three-ts",
   },
+  deployment: {
+    appId: "xixpzdusfkk5yyqf5uxr5eth",
+    autoUpdates: true,
+  },
 });
